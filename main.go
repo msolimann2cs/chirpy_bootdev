db550a0e5c2879f4a09a4978e92aa125bd9662fe
Mohamed Soliman <msolimann2cs@gmail.com>
2025-03-29 02:25:34 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"chirpy_bootdev/internal/database"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	// w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`, hits)))
+
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+
+	if cfg.platform != "dev" {
+		http.Error(w, "forbidden", http.StatusForbidden)
+		return
+	}
+
+	err := cfg.db.DeleteAllUsers(r.Context())
+	if err != nil {
+		http.Error(w, "failed to delete users", http.StatusInternalServerError)
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func customHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf(`{
+			"error": "Something went wrong"
+		}`)))
+		log.Printf("Error decoding parameters: %s", err)
+		return
+	}
+	if len(params.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write([]byte(fmt.Sprintf(`{
+		"error": "Chirp is too long"
+		}`)))
+		return
+	}
+
+	replacements := map[string]string{
+		"kerfuffle":      "****",
+		"sharbert":       "****",
+		"fornax":         "****",
+		"Kerfuffle":      "****",
+		"Fornsharbertax": "****",
+		"Fornax":         "****",
+	}
+	result := params.Body
+	for old, new := range replacements {
+		result = strings.ReplaceAll(result, old, new)
+	}
+
+	type returnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Cleaned_body string `json:"cleaned_body"`
+	}
+	respBody := returnVals{
+		Cleaned_body: result,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf(`{
+			"error": "Something went wrong"
+		}`)))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// an error will be thrown if the JSON is invalid or has the wrong types
+		// any missing fields will simply have their values in the struct set to their zero value
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf(`{
+			"error": "Something went wrong"
+		}`)))
+		log.Printf("Error decoding parameters: %s", err)
+		return
+	}
+
+	type returnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Id         uuid.UUID `json:"id"`
+		Created_at time.Time `json:"created_at"`
+		Updated_at time.Time `json:"updated_at"`
+		Email      string    `json:"email"`
+	}
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf(`{
+			"error": "Something went wrong"
+		}`)))
+		return
+	}
+	respBody := returnVals{
+		Id:         user.ID,
+		Created_at: user.CreatedAt,
+		Updated_at: user.UpdatedAt,
+		Email:      user.Email,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf(`{
+			"error": "Something went wrong"
+		}`)))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	w.Write(dat)
+}
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	platform := os.Getenv("PLATFORM")
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	// apiCfg := apiConfig{
+	// 	db: dbQueries,
+	// }
+
+	fmt.Println("Database connected and dbQueries ready!")
+
+	//--------------------
+	apiCfg := &apiConfig{
+		db:       dbQueries,
+		platform: platform,
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/users", apiCfg.createUser)
+	mux.HandleFunc("GET /api/healthz", customHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	//mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	// ----------------------------
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
